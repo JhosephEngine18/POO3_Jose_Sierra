17947e8e979ee40e3cd2700ce78c98283191be48
GameDevJ180 <josesielo1805@gmail.com>
2025-02-25 09:14:49 -0600

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+/*
+Universidad Amerike
+Author: José Sierra
+Practice#:3
+Date: 25/02/25
+Description: This code prints a common bunch of daily questions
+*/
+
+#include <iostream>
+using namespace std;
+
+string Name;
+string MLName;
+int Age;
+float Stature;
+
+int main ()
+{
+    cout<<"What is yout name?" << endl;
+    cin>>Name;
+    cout<<"What is your middle" << endl;
+    cin>>MLName;
+    cout<<"How old are you?" << endl;
+    cin>>Age;
+    cout<<"How tall are you?" << endl;
+    cin>>Stature;
+
+    cout<<"Your name is: "<< Name + " " + MLName + "\n";
+    cout<<"You are: "<< to_string(Age) + " Years old" "\n";
+    cout<<"Your stature is: " << to_string(Stature) + "\n";
+}
